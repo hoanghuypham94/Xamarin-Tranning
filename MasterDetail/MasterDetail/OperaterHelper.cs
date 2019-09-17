d082a115003d7feb8064be8e2cf4100c5ac1e788
huyph <huyph@runsystem.net>
2019-09-17 13:03:33 +0700

    Calculator va vi du layout

---
+++ MasterDetail/MasterDetail/OperaterHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    public class OperaterHelper : ContentPage
+    {
+        public static double Calculate(double value1, double value2, string myoperator)
+        {
+            double result = 0;
+            switch (myoperator)
+            {
+                case "/":
+                    result = value1 / value2;
+                    break;
+                case "X":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+
+            }
+            return result;
+        }
+    }
+}
+
---
+++ MasterDetail/MasterDetail/calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    public class calculator : ContentPage
+    {
+        public calculator()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
